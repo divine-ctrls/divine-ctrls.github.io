146f75ac90f78f26c48eb98ce18bd03103ace49e
Zharp <nikolaos.divine@gmail.com>
2025-11-21 15:50:59 -0500

    Add files via upload

---
+++ provideQuotes/scripts/myScript.js
@@ -0,0 +1,11 @@
+        const quotes = [
+            "\"To be, or not to be, that is the question.\"",
+            "\"Imagination is more important than knowledge.\"",
+            "\"If you want a definition of what a coward is, it’s needing to push a whole class of people down so that you can walk on top of them.\"",
+            "\"The secret of getting ahead is getting started.\"",
+            "\"Education is the most powerful weapon which you can use to change the world.\""
+        ];
+
+        function displayQuote(index) {
+            document.getElementById('quoteText').textContent = quotes[index];
+        }   
